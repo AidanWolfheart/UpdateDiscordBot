7f2117e947b45de41bcfa56ab671aa21e203dcfd
Kiril <17439949+AidanWolfheart@users.noreply.github.com>
2020-06-05 20:37:58 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+        echo "No version was given as a first parameter."
+else
+        echo "Terminating MusicBot"
+        pkill -f 'java.*JMusicBot'
+        ###
+        echo "Downloading new version"
+        wget https://github.com/jagrosh/MusicBot/releases/download/$1/JMusicBot>
+        ###
+        echo "Removing older version"
+        rm JMusicBot.jar
+        ###
+        echo "Updating to newer version"
+        mv JMusicBot-$1-Linux.jar JMusicBot.jar
+        sh ./run.sh
+fi
